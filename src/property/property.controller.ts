2622c26e73fcbddad2a4495b9449aebcf620b8ac
Md. Salauddin <salauddinahmed555@gmail.com>
2025-02-15 23:48:41 +0600

    added get param route

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
   @Get()
   findAll() {
     return 'All properties';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return id;
   }
   @Post()
   create() {
